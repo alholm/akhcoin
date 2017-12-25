c81c07e519eca5cea0f7033158f692d3e07725f7
Alexey Kholupko <alexey.kholupko@gmail.com>
2017-12-25 14:38:54 +0700

    Task:2 p2p basic exchange: sample communication with one peer

--- App.go
+++ App.go
@@ -10,6 +10,9 @@ import (
 	"context"
 	ma "github.com/multiformats/go-multiaddr"
 	"github.com/libp2p/go-libp2p-peer"
+	"bufio"
+	"os"
+	"strings"
 )
 
 type Node interface {
@@ -90,59 +93,66 @@ func main() {
 	//3) User-specified on the command line
 
 	// Parse some flags
-	remotePeer := *flag.String("a", "", "add peers - comma-separated list of peers addresses TODO <format>")
-	port := *flag.Int("p", 9000, "port where to start local host")
+	port := flag.Int("p", 9000, "port where to start local host")
+	remotePeerAddr := flag.String("a", "", "add peer address (format: <IP:port>)")
+	remotePeerID := flag.String("id", "", "add peer ID <format>")
 	flag.Parse()
-	fmt.Printf("Peer: %s, port: %d, bye!\n", remotePeer, port)
 
-	//host := p2p.MakeRandomHost(port)
-	//fmt.Println(host.Addrs()[0])
+	fmt.Printf("Peer: %s, port: %d, bye!\n", *remotePeerAddr, *port)
 
-	//host.Start()
-	//fmt.Printf("%s : %s\n", host.Addrs()[0], host.ID().Pretty())
-	//p2p.SetStreamHandler(host)
-	//defer host.Close()
+	host := p2p.MakeRandomHost(*port)
+
+	host.Start()
+	p2p.SetStreamHandler(host)
+
+	fmt.Printf("%s : %s\n", host.Addrs()[0], host.ID().Pretty())
+
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Enter text: ")
+		text, _ := reader.ReadString('\n')
+		if text == "exit\n" {
+			break
+		} else {
+			sendSmth(*remotePeerAddr, *remotePeerID, host)
+		}
+		fmt.Println(text)
+	}
+
 	//<-make(chan struct{}) // hang forever
 
-	host2 := p2p.MakeRandomHost(11000)
-	//fmt.Println(host.Addrs()[0])
+}
+func sendSmth(remotePeerAddr string, remotePeerID string, host p2p.MyHost) {
+	fmt.Printf("Sending to %s : %s;\n", remotePeerAddr, remotePeerID)
+	if len(remotePeerAddr) > 0 && len(remotePeerID) > 0 {
 
-	host2.Start()
-	fmt.Printf("%s : %s\n", host2.Addrs()[0], host2.ID().Pretty())
+		split := strings.Split(remotePeerAddr, ":")
+		addrStr := fmt.Sprintf("/ip4/%s/tcp/%s", split[0], split[1])
+		fmt.Printf("Sending to %s;\n", addrStr)
+		addr, _ := ma.NewMultiaddr(addrStr) //"/ip4/127.0.0.1/tcp/9000"
 
+		decID, err := peer.IDB58Decode(remotePeerID) //"QmcHWzenJP3B2jrvEDGk9Gdbw964LdrQCTZDDoT4nePbBU"
 
-	addr, _ := ma.NewMultiaddr("/ip4/127.0.0.1/tcp/9000")
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	decID, err := peer.IDB58Decode("QmVSzfqX9CLUkvA3z1h4HwvX1Ud9ipxbar3QE8MgReShpA")
-	//decID, err := peer.IDB58Decode(host.ID().Pretty())
+		peerInfo := peerstore.PeerInfo{ID: decID, Addrs: []ma.Multiaddr{addr}}
 
-	if err != nil {
-		log.Fatal(err)
+		host.AddPeer(&peerInfo)
+
+		// Create new stream from h1 to h2 and start the conversation
+		stream, err := host.NewStream(context.Background(), peerInfo.ID, "/akh/1.0.0")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		wrappedStream := p2p.WrapStream(stream)
+		// This sends the first message
+		p2p.SendMessage(0, wrappedStream)
+		// We keep the conversation on the created stream so we launch
+		// this to handle any responses
+		p2p.HandleStream(wrappedStream)
+		// When we are done, close the stream on our side and exit.
+		stream.Close()
 	}
-
-	peerInfo := peerstore.PeerInfo{ID: decID, Addrs: []ma.Multiaddr{addr}}
-
-	host2.AddPeer(&peerInfo)
-
-	// Create new stream from h1 to h2 and start the conversation
-	stream, err := host2.NewStream(context.Background(), peerInfo.ID, "/akh/1.0.0")
-	if err != nil {
-		log.Fatal(err)
-	}
-	wrappedStream := p2p.WrapStream(stream)
-	// This sends the first message
-	p2p.SendMessage(0, wrappedStream)
-	// We keep the conversation on the created stream so we launch
-	// this to handle any responses
-	p2p.HandleStream(wrappedStream)
-	// When we are done, close the stream on our side and exit.
-	stream.Close()
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	if len(remotePeer) == 0 {
-		log.Fatalln("Please provide at least one remotePeer to bind on with -a")
-	}
-
 }
